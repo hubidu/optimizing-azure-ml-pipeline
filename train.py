f9bee873b868a5ff1c92ca2146bb5b764778a9ff
Stefan Huber <stefan.huber3@baywa.de>
2023-02-16 15:52:00 +0100

    Add readme

--- train.py
+++ train.py
@@ -17,19 +17,24 @@ def clean_data(data):
 
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
+    
     jobs = pd.get_dummies(x_df.job, prefix="job")
     x_df.drop("job", inplace=True, axis=1)
     x_df = x_df.join(jobs)
+
     x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
     x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
     x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
     x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
+    
     contact = pd.get_dummies(x_df.contact, prefix="contact")
     x_df.drop("contact", inplace=True, axis=1)
     x_df = x_df.join(contact)
+    
     education = pd.get_dummies(x_df.education, prefix="education")
     x_df.drop("education", inplace=True, axis=1)
     x_df = x_df.join(education)
+    
     x_df["month"] = x_df.month.map(months)
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
@@ -37,8 +42,32 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df
 
-def main():
-    # Add arguments to script
+def main(args):
+    run = Run.get_context()
+
+    run.log("Regularization Strength:", float(args.C))
+    run.log("Max iterations:", int(args.max_iter))
+
+    ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+    
+    x, y = clean_data(ds)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", float(accuracy))
+
+    model_name = "model_reg_" + str(args.C) + "_maxit_" + str(args.max_iter) + ".pkl"
+    filename = "outputs/" + model_name
+    
+    joblib.dump(value=model, filename=filename)
+    run.upload_file(name=model_name, path_or_stream=filename)
+
+    run.complete()
+    
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
@@ -46,27 +75,4 @@ def main():
 
     args = parser.parse_args()
 
-    run = Run.get_context()
-
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
-
-    # TODO: Create TabularDataset using TabularDatasetFactory
-    # Data is located at:
-    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-    ds = ### YOUR CODE HERE ###
-    
-    x, y = clean_data(ds)
-
-    # TODO: Split data into train and test sets.
-
-    ### YOUR CODE HERE ###a
-
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
-
-if __name__ == '__main__':
-    main()
+    main(args)
